accfe858f26990d6f7b63c01b4437f084dcfe848
Craig Swanson <cswanson02@me.com>
2019-11-01 00:10:19 -0500

    created enum for Resulttype

---
+++ iTunesSearch/iTunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  iTunesSearch
+//
+//  Created by Craig Swanson on 11/1/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software = "Apps"
+    case musicTrack = "Music"
+    case movie = "Movies"
+}
