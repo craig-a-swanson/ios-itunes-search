58c243c7f8ee6adaa12916c194f0c527dc9d8be8
Craig Swanson <cswanson02@me.com>
2019-11-01 18:13:41 -0500

    Created table view cell controller

---
+++ iTunesSearch/iTunesSearch/SearchResultsTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchResultsTableViewCell.swift
+//  iTunesSearch
+//
+//  Created by Craig Swanson on 11/1/19.
+//  Copyright © 2019 Craig Swanson. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewCell: UITableViewCell {
+
+    // MARK: Properties
+    var searchResult: SearchResult? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: Outlets
+    @IBOutlet weak var mediaTitleLabel: UILabel!
+    @IBOutlet weak var mediaCreatorLabel: UILabel!
+    
+    
+    // MARK: Methods
+    func updateViews() {
+        
+    }
+    
+
+}
--- iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
+++ iTunesSearch/iTunesSearch/SearchResultsTableViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class SearchResultsTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    let searchResultsController = SearchResultController()
+    
+    
+    // MARK: Outlets
     @IBOutlet weak var mediaSelectionControl: UISegmentedControl!
     @IBOutlet weak var searchField: UISearchBar!
     
@@ -16,34 +22,27 @@ class SearchResultsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return searchResultsController.searchResults.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MediaResultsCell", for: indexPath) 
+        
+        
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
